435581ccc74cc96fe4dc8bdb6f863add67ae36d4
joey-tschirpy <joey.tschirpy@gmail.com>
2019-08-15 14:57:16 +0930

    Testing laser with line renderer

---
+++ Sun/Assets/Scripts/Laser.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private bool r, g, b;
+    private int power;
+
+    public Laser(bool red, bool green, bool blue, int power)
+    {
+        r = red;
+        g = green;
+        b = blue;
+        this.power = power;
+    }
+
+    public Laser(Laser laser)
+    {
+        r = laser.r;
+        g = laser.g;
+        b = laser.b;
+        power = laser.power;
+    }
+
+    public Color Color
+    {
+        get { return new Color(r ? 1 : 0, g ? 1 : 0, b ? 1 : 0); }
+    }
+
+    public static Laser Add(Laser laserOne, Laser laserTwo, bool addingPower = true)
+    {
+        bool r = laserOne.r || laserTwo.r;
+        bool g = laserOne.g || laserTwo.g;
+        bool b = laserOne.b || laserTwo.b;
+
+        // Add powers or take highest
+        int power = addingPower ? laserOne.power + laserTwo.power : Mathf.Max(laserOne.power, laserTwo.power);
+
+        return new Laser(r, g, b, power);
+    }
+}
---
+++ Sun/Assets/Scripts/LaserStarter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LaserStarter : MonoBehaviour
+{
+    [SerializeField]
+    private Transform exitPoint;
+
+    private Laser laser;
+    private LineRenderer laserRepresentation;
+    private RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserRepresentation = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(exitPoint.position, exitPoint.forward, Color.blue);
+
+        laserRepresentation.SetPosition(0, exitPoint.localPosition);
+
+        if (Physics.Raycast(exitPoint.position, exitPoint.forward, out hit))
+        {
+            laserRepresentation.SetPosition(1, hit.point - exitPoint.position);
+        }
+        else
+        {
+            laserRepresentation.SetPosition(1, exitPoint.forward * 10);
+        }
+    }
+}
